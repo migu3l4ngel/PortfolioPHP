5a91b771edb4800c874fb3c900f16c840f70270b
migu3l4ngel <hurtado_miguelangel@hotmail.com>
2022-04-08 01:07:06 -0500

    Detalles en CRUD. Redireccionamiento y muestreo de info en el Index

--- portfolio.php
+++ portfolio.php
@@ -15,20 +15,31 @@
         $objConexion = new Conexion();
         $sql = "INSERT INTO `proyectos` (`id`, `nombre`, `imagen`, `descripcion`) VALUES (NULL, '$nombre', '$imagen', '$descripcion');";
         $objConexion->Ejecutar($sql);
-    }
 
+        header("Location:portfolio.php");
+    }
+    
     if($_GET) {
         $id = $_GET['borrar'];
         $objConexion = new Conexion();
+        
+        $imagen = $objConexion->consultar("SELECT imagen FROM `proyectos` WHERE id=".$id);
+        unlink("img/".$imagen[0]["imagen"]);
+        
+        ///print_r($imagen);
+        ///print_r($imagen[0]["imagen"]);
+        
         $sql = "DELETE FROM `proyectos` WHERE `proyectos`.`id` = ".$_GET['borrar'];
         $objConexion->Ejecutar($sql);
+
+        header("Location:portfolio.php");
     }
     
     $objConexion = new Conexion();
     $proyectos = $objConexion->Consultar("SELECT * FROM `proyectos`");
     ///print_r($resultado);
-
-?>
+    
+    ?>
 <br/>
 <div class="container">
     <div class="row">
@@ -40,11 +51,11 @@
                 <div class="card-body">
                     <form  action="portfolio.php" method="post" enctype="multipart/form-data">
                         Nombre del proyecto:
-                        <input class="form-control" type="text" name="nombre" id=""><br/>
+                        <input class="form-control" type="text" name="nombre" required><br/>
                         Imagen del proyecto: 
-                        <input class="form-control" type="file" name="archivo" id=""><br/>
+                        <input class="form-control" type="file" name="archivo" required><br/>
                         Descripción: 
-                        <textarea class="form-control" name="descripcion" rows="3"></textarea><br/>
+                        <textarea class="form-control" name="descripcion" rows="3" required></textarea><br/>
                     
                         <input class="btn btn-success" type="submit" value="Enviar">
                     </form>
@@ -61,7 +72,7 @@
                     <caption></caption>
                         <thead>
                             <tr>
-                                <th>ID:</th>
+                                <th>ID:</th> 
                                 <th>Nombre: </th>
                                 <th>Imagen: </th>
                                 <th>Descripción: </th>
@@ -72,7 +83,9 @@
                                 <tr>
                                     <td><?= $proyecto['id'] ?></td>
                                     <td><?= $proyecto['nombre'] ?></td>
-                                    <td><?= $proyecto['imagen'] ?></td>
+                                    <td>
+                                        <img  height="100" src="img/<?= $proyecto['imagen'] ?>" alt="<?= $proyecto['imagen'] ?>" title="<?= $proyecto['imagen'] ?>">
+                                    </td>
                                     <td><?= $proyecto['descripcion'] ?></td>
                                     <td><a class="btn btn-danger" href="?borrar= <?= $proyecto['id'] ?>" >Eliminar</a></td>
                                 </tr>
